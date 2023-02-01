e7b97d6864fa14371576d9b5202ab9204a659acd
Ricky D'Cruze <rickystanley76@gmail.com>
2023-02-01 11:06:21 +0100

    Add files via upload
    Updated with character-normalized perplexity

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 st.header("Text Generation with GPT-SW3 Model of AI Sweden")
 
+
 # Initialize Tokenizer & Model
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True, auth_token=organization_token)
 model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -25,10 +26,22 @@ model.to(device)
 
 prompt = st.text_input("Enter your prompt:")
 
+
+# Custom function to calculate character normalised perplexity
+def calculate_char_norm_perplexity(model, tokenizer, prompt):
+    input_ids = torch.tensor(tokenizer.encode(prompt, return_tensors='pt')).unsqueeze(0)
+    outputs = model(input_ids)  
+    loss = outputs[0].mean()
+    perplexity = torch.exp(loss)
+    char_norm_perplexity = perplexity / len(prompt)
+    return char_norm_perplexity.item()
+
+
+
 st.sidebar.title("Settings")
 
 max_new_tokens = st.sidebar.number_input("Max New Tokens", value=100, min_value=0, max_value=1000)
-do_sample= st.sidebar.checkbox("Do Sample?", value=True) # Whether or not to use sampling 
+do_sample= st.sidebar.checkbox("Do Sample?", value=True) # Whether or not to use samplingp 
 temperature =st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.6) #The value used to module the next token probabilities
 top_p= st.sidebar.slider("Top P", min_value=0.0, max_value=1.0, value=1.0) #  If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation
 
@@ -42,27 +55,18 @@ if st.button("Generate"):
     ## Calculating the time needed to generate the text
     st.write("Time taken: {:.4f} seconds".format(end_time - start_time))
 
-    ##BLEU Score Calculation
-    prompt = prompt.split()
-    generated = generated.split()
-    bleu_score = sentence_bleu([prompt], generated)
-    st.write("BLEU Score(1 is perfect match):", bleu_score)
+    ##calculates the character normalised perplexity
+    char_norm_perplexity = calculate_char_norm_perplexity(model, tokenizer, prompt)
+    #st.write("Character Normalised Perplexity: {:.4f}".format(char_norm_perplexity))
+    st.markdown(f"<p style='color: green'>Character Normalised Perplexity: {char_norm_perplexity:.4f}</p>", unsafe_allow_html=True)
 
-    with st.expander("What is BLEU score? "):
-        st.write("BLEU (Bilingual Evaluation Understudy) is a widely used automatic evaluation metric in Natural Language Processing (NLP) "+
-                 "for evaluating the quality of machine-generated text in comparison to reference human-generated text. It was developed  "+
-                 "by Kishore Papineni et al. in 2002. ")
-        st.write("BLEU is a comparison-based evaluation metric that calculates the n-gram precision between a machine-generated text and  "+
-                 "a reference text.")
-        st.write("The interpretation of BLEU score is based on the number of matching n-grams between the generated text and reference text. "+
-                 "The score ranges from 0 to 1, with 1 indicating a perfect match. A higher score indicates that the generated text is closer  "+
-                 "to the reference text. The exact interpretation of the score varies based on the order of n-grams (i.e. 1-gram, 2-gram, "+
-                 "3-gram, etc.) used in the calculation. ")  
-        st.write("For example, a BLEU score of 0.4 means that 40% of the n-grams in the generated text match the n-grams in the reference "+
-                 "text. A score of 0.6 means 60% match, and so on. The exact value of the score will depend on the specifics of the task,  "+
-                 "the quality of the reference text, and the performance of the text generation model.")   
-        st.write("https://towardsdatascience.com/how-to-evaluate-text-generation-models-metrics-for-automatic-evaluation-of-nlp-models-e1c251b04ec1 ")     
-
+    with st.expander("What is character normalised perplexity? "):
+        st.write("Character Normalized Perplexity is a measure of the quality of a text generation model. It represents how "+
+                 "well the model can predict the next character in a sequence given the previous characters. It is a way of "+
+                 "evaluating the model's ability to generate coherent and meaningful sequences of characters. The score is "+
+                 "calculated by dividing the total number of characters by the total log likelihood of the model's predictions. " +
+                 "A lower score indicates a better model, as it means the model is able to predict the next character with high accuracy. ")
+        st.write("https://medium.com/towards-data-science/perplexity-in-language-models-87a196019a94 ")
     
 
 
